37fcb0f020894e33795ca1155ebf2ab9fd2be90f
Connor Peebles <connorpeebles93@gmail.com>
2018-10-24 15:47:34 +0000

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  if (array.length === 0) {
+    return 0;
+  }
+  else if (Array.isArray(array[0])) {
+    return sumItems(array[0]) + sumItems(array.slice(1));
+  }
+  else {
+    return array[0] + sumItems(array.slice(1));
+  }
 }
 
 module.exports = sumItems;
